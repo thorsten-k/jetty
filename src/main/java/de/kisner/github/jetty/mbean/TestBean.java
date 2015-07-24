a448d4a200e9cdc87b408fd11511b0223c611181
Thorsten <thorsten-k>
2015-07-24 15:34:41 +0100

    JSF showcase added

---
+++ src/main/java/de/kisner/github/jetty/mbean/TestBean.java
@@ -0,0 +1,29 @@
+package de.kisner.github.jetty.mbean;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ManagedBean
+@ViewScoped
+public class TestBean implements Serializable
+{
+	private static final long serialVersionUID = 1;
+	final static Logger logger = LoggerFactory.getLogger(TestBean.class);
+	
+	private String value;
+	public String getValue() {return value;}
+	public void setValue(String value) {this.value = value;}
+
+	@PostConstruct
+	public void init()
+	{
+		logger.info("Init!");
+		
+	}
+}
