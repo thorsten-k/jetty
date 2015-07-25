294b7f5a2ac7dba3ae0b223c47f505c5e85b59a6
Thorsten <t.kisner@web.de>
2015-07-25 12:47:28 +0100

    Test Info added

--- src/main/java/de/kisner/github/jetty/JettyServer.java
+++ src/main/java/de/kisner/github/jetty/JettyServer.java
@@ -6,17 +6,21 @@ import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.kisner.github.Bootstrap;
 
 public class JettyServer 
 {
+	final static Logger logger = LoggerFactory.getLogger(JettyServer.class);
+	
 	public JettyServer() throws Exception
 	{
 		Server server = new Server(8080);
 
 		ResourceHandler fileHandler = new ResourceHandler();
-		fileHandler.setResourceBase("./src");
+		fileHandler.setResourceBase("./src/main/resources");
  
 		ContextHandler ctx = new ContextHandler("/files");
 		ctx.setHandler(fileHandler);
@@ -24,7 +28,7 @@ public class JettyServer
 		WebAppContext context = new WebAppContext();
 		context.setDescriptor("./src/main/webapp/WEB-INF/web.xml");
 		context.setResourceBase("./src/main/webapp");
-		context.setContextPath("/cast");
+		context.setContextPath("/app");
 		context.setParentLoaderPriority(true);            
  
 		HandlerList handlers = new HandlerList();
@@ -38,6 +42,12 @@ public class JettyServer
 	public static void main(String args[]) throws Exception
 	{		
 		Bootstrap.init();
+		
+		logger.info("Test with:");
+		logger.info("\thttp://localhost:8080/app/index.jsf");
+		logger.info("\thttp://localhost:8080/app/rest");
+		logger.info("\thttp://localhost:8080/files/test.txt");
+		
 		new JettyServer();
 	}
 }
