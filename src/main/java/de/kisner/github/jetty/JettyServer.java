4a548753065b21c26dac90f5f3aa7757d03c32de
Thorsten <t.kisner@web.de>
2015-07-25 09:59:41 +0100

    code formatting improved

--- src/main/java/de/kisner/github/jetty/JettyServer.java
+++ src/main/java/de/kisner/github/jetty/JettyServer.java
@@ -13,28 +13,26 @@ public class JettyServer
 {
 	public JettyServer() throws Exception
 	{
-		 Server server = new Server(8080);
+		Server server = new Server(8080);
 
-		 ResourceHandler fileHandler = new ResourceHandler();
-		 fileHandler.setResourceBase("./src");
-		 
-		 ContextHandler ctx = new ContextHandler("/files"); /* the server uri path */
-		 ctx.setHandler(fileHandler);
-		 
-		 
-         WebAppContext context = new WebAppContext();
-         context.setDescriptor("./src/main/webapp/WEB-INF/web.xml");
-         context.setResourceBase("./src/main/webapp");
-         context.setContextPath("/cast");
-         context.setParentLoaderPriority(true);            
-         
-         HandlerList handlers = new HandlerList();
-         handlers.setHandlers(new Handler[] { context, ctx});
-         server.setHandler(handlers);
+		ResourceHandler fileHandler = new ResourceHandler();
+		fileHandler.setResourceBase("./src");
+ 
+		ContextHandler ctx = new ContextHandler("/files"); /* the server uri path */
+		ctx.setHandler(fileHandler);
+ 
+		WebAppContext context = new WebAppContext();
+		context.setDescriptor("./src/main/webapp/WEB-INF/web.xml");
+		context.setResourceBase("./src/main/webapp");
+		context.setContextPath("/cast");
+		context.setParentLoaderPriority(true);            
+ 
+		HandlerList handlers = new HandlerList();
+		handlers.setHandlers(new Handler[] { context, ctx});
+		server.setHandler(handlers);
 
-         server.start();
-         server.join();
-
+		server.start();
+		server.join();
 	}
 	
 	public static void main(String args[]) throws Exception
