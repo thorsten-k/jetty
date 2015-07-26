19f06c8361863333d46a720b95d6b8035f37fb6e
Thorsten <t.kisner@web.de>
2015-07-26 13:52:52 +0100

    Servlet example added

--- src/main/java/de/kisner/github/jetty/JettyServer.java
+++ src/main/java/de/kisner/github/jetty/JettyServer.java
@@ -5,11 +5,14 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.kisner.github.Bootstrap;
+import de.kisner.github.jetty.servlet.HelloServlet;
 
 public class JettyServer 
 {
@@ -29,10 +32,14 @@ public class JettyServer
 		context.setDescriptor("./src/main/webapp/WEB-INF/web.xml");
 		context.setResourceBase("./src/main/webapp");
 		context.setContextPath("/app");
-		context.setParentLoaderPriority(true);            
+		context.setParentLoaderPriority(true);
+		
+		ServletContextHandler ctxServlet = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		ctxServlet.setContextPath("/servlet");
+		ctxServlet.addServlet(new ServletHolder(new HelloServlet()),"/hello/*");
  
 		HandlerList handlers = new HandlerList();
-		handlers.setHandlers(new Handler[] {context, ctx});
+		handlers.setHandlers(new Handler[] {context, ctx, ctxServlet});
 		server.setHandler(handlers);
 
 		server.start();
@@ -47,6 +54,7 @@ public class JettyServer
 		logger.info("\thttp://localhost:8080/app/index.jsf");
 		logger.info("\thttp://localhost:8080/app/rest");
 		logger.info("\thttp://localhost:8080/files/test.txt");
+		logger.info("\thttp://localhost:8080/servlet/hello/x");
 		
 		new JettyServer();
 	}
